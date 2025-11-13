cdb65536c55ce7719937ad100c5fd8e289f58df0
sethi353 <sethisaha56@gmail.com>
2025-11-13 12:58:11 +0600

    Updated index.js file

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use('/api/books', bookRoutes);
 app.get('/test', (req, res) => res.send(" Backend working"));
 
 // Catch-all 404
-app.use((req, res) => res.status(404).send("Route is  not found"));
+app.use((req, res) => res.status(404).send("Route not found"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
